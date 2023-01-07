f374398bbeb7172cb0b913e80ee5c0f6d8cde042
Francisco de Assis <contato@franciscoprog.com>
2023-01-07 13:03:38 -0300

    estilizando pedidos

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import { canSRRAuth } from "../../utils/canSRRAuth"
 import Head from 'next/head'
 import { Header } from "../../components/Header"
+import { FiRefreshCcw } from "react-icons/fi"
+
+import styles from './styles.module.scss'
 
 
 export default function DashBoard(){
@@ -12,9 +15,26 @@ export default function DashBoard(){
 
         <div>
         <Header/>
-            <h1>Home</h1>
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1 className={styles.titulo}>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw size={25} color="#00FA90"/>
+                    </button>
+                </div> 
+
+                <article className={styles.listOrders}>
+
+                    <section className={styles.orderItem}>
+                        <button>
+                            <div className={styles.tag}></div>
+                            <span>Mesa 30</span>
+                        </button>
+                    </section>
+
+                </article>
+            </main>
         </div>
-        
         </>
     )
 }
